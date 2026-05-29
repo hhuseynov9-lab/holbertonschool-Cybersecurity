3a49713d4b802576b7ef55e20e6f0bbbe7127a3e
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-05-29 09:57:44 +0000

    task 3

---
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+IFS='.' read -r -a o <<< "$1" && printf "%08d.%08d.%08d.%08d\n" $(echo "obase=2; ${o[0]}" | bc) $(echo "obase=2; ${o[1]}" | bc) $(echo "obase=2; ${o[2]}" | bc) $(echo "obase=2; ${o[3]}" | bc)
